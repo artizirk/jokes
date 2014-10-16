71b5653db803f3ca841adc11139346edb570e83a
Mark Vainomaa <mikroskeem@gmail.com>
2014-10-16 14:30:37 +0300

    Tweaked html and txt reading

--- app.py
+++ app.py
@@ -3,8 +3,7 @@
 import base64
 import os
 
-html = """
-<!DOCTYPE html>
+html = """<!DOCTYPE html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -19,21 +18,17 @@ html = """
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-
-        <br>
+        <div id="header">
         <h1>The best IRC jokes in the Internet</h1>
-
+        </div>
         <hr>
         {posts}
         <hr>
-        <small><a href="#" style="color:black;">Source code</a></small>,  <small>now press F5</small>
+        <small><a href="https://github.com/arti95/jokes" style="color:black;">Source code</a></small>
     </body>
-</html>
-"""
+</html>"""
 
-style="""
-/*i cant style*/
-body {
+style="""body {
     max-width: 50em;
     left: 50%;
     margin-left: auto;
@@ -41,22 +36,15 @@ body {
     padding-left: 1em;
     padding-right: 1em;
 }
-
 h2>small {
     font-size: initial;
     font-weight: normal;
 }
-"""
-
-post_template ="""
-<h2>{title} <small>({tag})</small></h2>
-<p><pre>
-{post}
-</pre></p>
-"""
-
-favicon = 'AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAD/hAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABEREQAAAAABEREREAAAAAEQAAEQAAAAARAAARAAAAAAAAABEAAAAAAAAAEQAAAAAAAAARAAAAAAAAABEAAAAAAAAAEQAAAAAAAAARAAAAAAAAABEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-
+pre {
+    white-space: pre-wrap;
+}"""
+post_template ='<div id="{title}"><a name="#{title}"><h2>{title} <small>({tag})</small></h2></a><p><pre>{post}</pre></p></div>'
+favicon = "AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAD/hAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABEREQAAAAABEREREAAAAAEQAAEQAAAAARAAARAAAAAAAAABEAAAAAAAAAEQAAAAAAAAARAAAAAAAAABEAAAAAAAAAEQAAAAAAAAARAAAAAAAAABEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 
 def application(env, start_response):
     if env["PATH_INFO"] == "/favicon.ico":
@@ -66,7 +54,6 @@ def application(env, start_response):
     if env["PATH_INFO"] == "/style.css":
         start_response('200 OK', [('Content-Type', 'text/css')])
         return [style.encode()]
-
     start_response('200 OK', [('Content-Type', 'text/html')])
     posts = []
     files = []
@@ -77,15 +64,13 @@ def application(env, start_response):
 
     for joke in files:
         f = open("jokes/{}/{}".format(joke[0], joke[1]), "rb")
-        p = post_template.format(title=joke[1], tag=joke[0], post=f.read().decode())
+        p = post_template.format(title=joke[1], tag=joke[0], post=f.read().decode().replace("<", "&lt;").replace(">", "&gt;"))
         f.close()
         posts.append(p)
     return [html.format(posts=''.join(posts)).encode()]
 
-
 if __name__ == "__main__":
     from wsgiref.simple_server import make_server
-
-    httpd = make_server('localhost', 8080, application)
-    print("Serving on http://localhost:8080/")
+    httpd = make_server('localhost', 58300, application)
+    print("Serving on http://localhost:58300/")
     httpd.serve_forever()
